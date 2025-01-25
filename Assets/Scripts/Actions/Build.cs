e0b1b3346c8afe029f9743c6632505cdbabec2b3
Piotr Telka <piotr.telka1@gmail.com>
2025-01-25 22:49:20 +0100

    ttt

--- Assets/Scripts/Actions/Build.cs
+++ Assets/Scripts/Actions/Build.cs
@@ -135,9 +135,11 @@ public class Build : IAction
 
     void BuildSkyscraperEffects(MarketManager market, Node node)
     {
+        var types = new List<HexTopsType>();
+        types.Add(HexTopsType.House);
         foreach (var neighbour in node.GetNeighbours())
         {
-            neighbour.GetComponent<HexController>().AddEffect(new ValueMultiplayer(0.8f));
+            neighbour.GetComponent<HexController>().AddEffect(new ValueMultiplayer(0.8f, types));
         }
     }
 }
